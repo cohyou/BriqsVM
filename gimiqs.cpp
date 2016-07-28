3a42e606b7c444e6985a602853a789861e5ecddb
OKAZAKI Yasuo <yasuo_okazaki@fandmnet.com>
2016-07-28 18:18:30 +0900

    making ADLC ADHC command, but buggy...

--- gimiqs.cpp
+++ gimiqs.cpp
@@ -1,24 +1,8 @@
 #include "gimiqs.hpp"
 
 #include <vector>
-#include <string>
 #include <istream>
-#include <iostream>
-// #include <iomanip>
 #include <type_traits>
-
-#define _BYTE1(x) (  x        & 0xFF )
-#define _BYTE2(x) ( (x >>  8) & 0xFF )
-#define _BYTE3(x) ( (x >> 16) & 0xFF )
-#define _BYTE4(x) ( (x >> 24) & 0xFF )
-#define _BYTE5(x) ( (x >> 32) & 0xFF )
-#define _BYTE6(x) ( (x >> 40) & 0xFF )
-#define _BYTE7(x) ( (x >> 48) & 0xFF )
-#define _BYTE8(x) ( (x >> 56) & 0xFF )
-
-#define BYTE_SWAP_16(x) ((uint16_t)( _BYTE1(x)<<8 | _BYTE2(x) ))
-#define BYTE_SWAP_32(x) ((uint32_t)( _BYTE1(x)<<24 | _BYTE2(x)<<16 | _BYTE3(x)<<8 | _BYTE4(x) ))
-#define BYTE_SWAP_64(x) ((uint64_t)( _BYTE1(x)<<56 | _BYTE2(x)<<48 | _BYTE3(x)<<40 | _BYTE4(x)<<32 | _BYTE5(x)<<24 | _BYTE6(x)<<16 | _BYTE7(x)<<8 | _BYTE8(x) ))
 
 std::vector<std::string> split(const std::string &str, char sep) {
     std::vector<std::string> v;
@@ -41,6 +25,62 @@ std::vector<std::string> split(const std::string &str, char sep) {
     return v;
 }
 
+namespace gimiqs {
+    std::string show_briq(Briq& b) {
+        std::string ret;
+
+        if (b.LLLL == 0x00) {
+            switch (b.LLLH) {
+                case 0:
+                ret = "N";
+                break;
+                case -1: {
+                    std::stringstream ss;
+                    ss << "MAX<" << BYTE_SWAP_32(b.LH) << ">";
+                    ret = ss.str();
+                }
+                return ret;
+                default:
+                break;
+            }
+
+        } else if (b.LLLL == -1) {
+            ret = "SIG";
+            switch (b.LLLH) {
+                case -1:
+                ret += "<INTERPRETER>";
+                break;
+                default:
+                break;
+            }
+        } else {
+            std::string annot_str {b.LLLL};
+            ret = "annot is [" + annot_str + "] ";
+            return ret;
+        }
+
+        if (b.LH > 0 || b.HH > 0) ret += "<";
+
+        if (b.LH > 0) {
+            std::stringstream ss;
+            ss << BYTE_SWAP_32(b.LH);
+            ret += "P:" + ss.str();
+        }
+
+        if (b.LH > 0 && b.HH > 0) ret += " ";
+
+        if (b.HH > 0) {
+            std::stringstream ss;
+            ss << BYTE_SWAP_32(b.HH);
+            ret += "Q:" + ss.str();
+        }
+
+        if (b.LH > 0 || b.HH > 0) ret += ">";
+
+        return ret;
+    }
+} // namespace gimiqs
+
 void test_briq(gimiqs::Baseplate& bp) {
     std::cout << "hello, piq." << std::endl;
 
@@ -59,15 +99,123 @@ void test_briq(gimiqs::Baseplate& bp) {
     std::cout << "Briq is " << (std::is_pod<gimiqs::Briq>::value ? "POD!" : "not POD!") << std::endl;
 }
 
+bool check_args_size(std::vector<std::string> tokens, int required_arg_count) {
+    if (tokens.size() != required_arg_count + 1) {
+        std::cout << "sorry, rmbr command requires " << required_arg_count << " arg... plz try again. " << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool is_digit_string(std::string s) {
+    if (s.empty()) return false;
+    for (auto it = s.begin(); it != s.end(); ++it) {
+        if (!isdigit(*it)) return false;
+    }
+    return true;
+}
+
+bool is_hex_digit_string(std::string s) {
+    if (s.empty()) return false;
+    for (auto it = s.begin(); it != s.end(); ++it) {
+        if (!isxdigit(*it)) return false;
+    }
+    return true;
+}
+
 void parse_assembler(gimiqs::Baseplate& bp, std::vector<std::string> tokens) {
     if (tokens[0] == "quit") {
         exit(0);
     } else if (tokens[0] == "mkbc") {
         if (tokens.size() != 2) {
-            std::cout << "sorry, skbc command requires 1 arg... plz try again. " << std::endl;
+            std::cout << "sorry, mkbc command requires 1 arg... plz try again. " << std::endl;
         } else {
             std::cout << "you wanna make bucket of #" << tokens[1] << ", i see." << std::endl;
+            bp.make_bucket(std::stoi(tokens[1]));
         }
+    } else if (tokens[0] == "rmbc") {
+        if (tokens.size() != 2) {
+            std::cout << "sorry, rmbc command requires 1 arg... plz try again. " << std::endl;
+        } else {
+            bp.slay_bucket(std::stoi(tokens[1]));
+        }
+    } else if (tokens[0] == "mkbr") {
+        if (tokens.size() != 2) {
+            std::cout << "sorry, mkbr command requires 1 arg... plz try again. " << std::endl;
+        } else {
+            bp.at(std::stoi(tokens[1])).push();
+        }
+    } else if (tokens[0] == "rmbr") {
+        if (tokens.size() != 2) {
+            std::cout << "sorry, rmbr command requires 1 arg... plz try again. " << std::endl;
+        } else {
+            bp.at(std::stoi(tokens[1])).pop();
+        }
+    } else if (tokens[0] == "adlc" || tokens[0] == "adhc") {
+        if (check_args_size(tokens, 2)) {
+            // first arg is briq index
+            // <bucket_number>:<briq_index> or <bucket_number>
+            auto briq_index_tokens = split(tokens[1], ':');
+            gimiqs::Briq& target_briq = bp.at(-1).at(0);
+            short bucket_number;
+            switch (briq_index_tokens.size()) {
+                case 1: {
+                    // should be formula <bucket_number>
+                    std::string arg1 = briq_index_tokens[0];
+                    if (is_digit_string(arg1)) {
+                        // std::cout << "arg1[" << arg1 << "]" << std::endl;
+                        bucket_number = std::stol(arg1);
+                    }
+                    target_briq = bp.at(bucket_number).top();
+                    break;
+                }
+
+                case 2: {
+                    int briq_index;
+
+                    // should be formula <bucket_number>:<briq_index>
+                    std::string arg1 = briq_index_tokens[0];
+                    if (is_digit_string(arg1)) {
+                        bucket_number = std::stol(arg1);
+                    }
+                    std::string arg2 = briq_index_tokens[1];
+                    if (is_digit_string(arg2)) {
+                        briq_index = std::stol(arg2);
+                    }
+                    target_briq = bp.at(bucket_number).at(briq_index);
+                    break;
+                }
+
+                default:
+                break;
+            }
+
+            auto token2 = tokens[2];
+            if (is_digit_string(token2)) {
+                // std::cout << token2 << std::endl;
+                if (tokens[0] == "adlc") {
+                    target_briq.L = BYTE_SWAP_64(std::stol(token2));
+                } else if (tokens[0] == "adhc") {
+                    target_briq.H = BYTE_SWAP_64(std::stol(token2));
+                }
+            }
+
+            // std::cout << "check " << std::stol("0xFF", nullptr, 16) << std::endl;
+        }
+    } else if (tokens[0] == "adll") {
+    } else if (tokens[0] == "adlh") {
+    } else if (tokens[0] == "adhl") {
+    } else if (tokens[0] == "adhh") {
+    } else if (tokens[0] == "orlc") {
+    } else if (tokens[0] == "orhc") {
+    } else if (tokens[0] == "orll") {
+    } else if (tokens[0] == "orlh") {
+    } else if (tokens[0] == "orhl") {
+    } else if (tokens[0] == "orhh") {
+    } else if (tokens[0] == "notc") {
+    } else if (tokens[0] == "noth") {
+    } else if (tokens[0] == "show") {
+        ; // do nothing (= only print current state of baseplate)
     } else {
         std::cout << "sorry, i can not get what you mean... ('" << tokens[0] << "'). plz try again. " << std::endl;
     }
@@ -76,12 +224,16 @@ void parse_assembler(gimiqs::Baseplate& bp, std::vector<std::string> tokens) {
 int main() {
     gimiqs::Baseplate bp;
 
-    test_briq(bp);
+    // test_briq(bp);
+
+    // bp.at(-1)
 
     while (true) {
         char buffered_string[256];
+        std::cout << "@|| ";
         std::cin.getline(buffered_string, sizeof(buffered_string));
         std::string s { buffered_string };
         parse_assembler(bp, split(s, ' '));
+        std::cout << bp.to_s() << std::endl;
     }
 }
--- gimiqs.hpp
+++ gimiqs.hpp
@@ -1,5 +1,23 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
 #include <vector>
 #include <map>
+#include <sstream>
+
+#define _BYTE1(x) (  x        & 0xFF )
+#define _BYTE2(x) ( (x >>  8) & 0xFF )
+#define _BYTE3(x) ( (x >> 16) & 0xFF )
+#define _BYTE4(x) ( (x >> 24) & 0xFF )
+#define _BYTE5(x) ( (x >> 32) & 0xFF )
+#define _BYTE6(x) ( (x >> 40) & 0xFF )
+#define _BYTE7(x) ( (x >> 48) & 0xFF )
+#define _BYTE8(x) ( (x >> 56) & 0xFF )
+
+#define BYTE_SWAP_16(x) ((uint16_t)( _BYTE1(x)<<8 | _BYTE2(x) ))
+#define BYTE_SWAP_32(x) ((uint32_t)( _BYTE1(x)<<24 | _BYTE2(x)<<16 | _BYTE3(x)<<8 | _BYTE4(x) ))
+#define BYTE_SWAP_64(x) ((uint64_t)( _BYTE1(x)<<56 | _BYTE2(x)<<48 | _BYTE3(x)<<40 | _BYTE4(x)<<32 | _BYTE5(x)<<24 | _BYTE6(x)<<16 | _BYTE7(x)<<8 | _BYTE8(x) ))
+
 
 namespace gimiqs {
 
@@ -37,24 +55,60 @@ namespace gimiqs {
                                               struct { char HHHL, HHHH; }; }; }; }; }; };
     }; // struct Briq
 
+    std::string show_briq(Briq& b);
+
     class Bucket {
         std::vector<Briq> briqs;
     public:
         Bucket() {
             this->push();
+            briqs[0].LLL = 0xFF00; // little endian ... [0x00 0xFF]
+            briqs[0].LH = 0;
         }
 
         void push() {
             Briq b {0};
             briqs.push_back(b);
+
+            // increment max_index
+            int max_index = this->max_index() + 1;
+            briqs[0].LH = BYTE_SWAP_32(max_index);
         }
 
         void pop() {
+            // can not remove first briq of bucket
+            // (it has index of last briq)
+            if (briqs.size() == 1) {
+                std::cout << "sorry, the bucket is already empty. plz try again." << std::endl;
+                return;
+            }
             briqs.pop_back();
+
+            // decrement max_index
+            int max_index = this->max_index() - 1;
+            briqs[0].LH = BYTE_SWAP_32(max_index);
         }
 
         Briq& at(int index) {
             return briqs.at(index);
+        }
+
+        std::string to_s() {
+            std::stringstream ss;
+            for (auto it = briqs.begin(); it != briqs.end(); ++it) {
+                ss << show_briq(*it) << " ";
+            }
+            std::string s = ss.str();
+            s.pop_back();
+            return s;
+        }
+
+        int max_index() {
+            return BYTE_SWAP_32(briqs[0].LH);
+        }
+
+        Briq& top() {
+            return briqs.back();
         }
     }; // class Bucket
 
@@ -63,6 +117,7 @@ namespace gimiqs {
     public:
         Baseplate() {
             this->make_bucket(0xFFFF);
+            buckets[0xFFFF].at(0).LLL = 0xFFFF;
         }
 
         void make_bucket(short index) {
@@ -70,8 +125,23 @@ namespace gimiqs {
             buckets[index] = bc;
         }
 
+        void slay_bucket(short index) {
+            buckets.erase(index);
+        }
+
         Bucket& at(short index) {
             return buckets[index];
+        }
+
+        std::string to_s() {
+            std::stringstream ss;
+            for (auto it = buckets.begin(); it != buckets.end(); ++it) {
+                // bucket number
+                ss << "[" << std::hex << std::setw(4) << std::setfill('0') << std::uppercase << it->first << "]";
+                // content of bucket
+                ss << "{" << it->second.to_s() << "}" << "\n";
+            }
+            return ss.str();
         }
     }; // class Baseplate
 
